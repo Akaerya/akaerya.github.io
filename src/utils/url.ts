659ecf229c87ddd1cba8caba030164606a2d5478
yookers <miyookerie@gmail.com>
2026-04-08 17:16:21 -0400

    style stuff

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,3 +3,17 @@ export const base = import.meta.env.BASE_URL.replace(/\/$/, "");
 export function url(path: string) {
   return `${base}${path.startsWith("/") ? path : `/${path}`}`;
 }
+
+export function getImageUrl(path: string): string {
+  let formattedPath = path.replace(/\.png$/, ".webp");
+  const lastSlashIndex = formattedPath.lastIndexOf('/');
+  
+  // Lowercase before the last slash
+  if (lastSlashIndex !== -1) {
+    const directory = formattedPath.slice(0, lastSlashIndex).toLowerCase();
+    const filename = formattedPath.slice(lastSlashIndex);
+    formattedPath = directory + filename;
+  }
+  
+  return `https://raw.githubusercontent.com/FortOfFans/HSR/main/${formattedPath}`;
+}
