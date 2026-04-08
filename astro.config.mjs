81ed64dddbf73d38c26cb404d74b50c855032066
yookers <miyookerie@gmail.com>
2026-04-08 14:34:48 -0400

    remove base path for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://akaerya.github.io",
-  base: "/yuko",
+  // base: "/yuko",
 	integrations: [react()],
 	vite: {
 		plugins: [tailwindcss()],
