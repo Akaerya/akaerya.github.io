45aa25db2cb3c448872941ea0fd04e370cd7c6fd
yookers <miyookerie@gmail.com>
2026-04-10 16:59:22 -0400

    sanity sanity

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,7 +2,7 @@ import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_PROJECT_ID || "q1u16tf6",
-    dataset: process.env.SANITY_DATASET || "production",
+    projectId: "q1u16tf6",
+    dataset: "production",
   },
 });
--- sanity.config.ts
+++ sanity.config.ts
@@ -3,8 +3,8 @@ import { structureTool } from "sanity/structure";
 import { schema } from "./src/sanity/schemaTypes";
 
 export default defineConfig({
-  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID || "q1u16tf6",
-  dataset: import.meta.env.PUBLIC_SANITY_DATASET || "production",
+  projectId: "q1u16tf6",
+  dataset: "production",
   plugins: [structureTool()],
   schema,
 });
